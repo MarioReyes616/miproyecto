081263120e004092bdfe849d43a4f482ae03b5a0
MarioReyes <mario.reyes389@std.uni.edu.ni>
2025-06-24 20:32:33 -0600

    H

--- HolaMundo.java
+++ HolaMundo.java
@@ -9,17 +9,21 @@ public class HolaMundo {
 
         int multi= a*b;
 
-        System.out.println("Hello, Teacher");
+        System.out.println("Hello, Profe");
+
+        
 
         System.out.println("Hola, "+nombre+"!" );
         
-        System.out.println("La suma de "+a+"*"+b+" es:"+multi);
+        System.out.println("La multiplicacións de "+a+"*"+b+" es:"+multi);
 
         System.out.println("Adiós");
         
         System.out.println("Mario José Reyes Rodriguez");
 
         System.out.println("2025-2802U");
+
+        System.out.println("Hasta luego");
 
       
 
